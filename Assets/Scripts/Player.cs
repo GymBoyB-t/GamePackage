bbefb83211c63293c829164560580424e177dc54
GymBoyB-t <troy.boehne@gmail.com>
2021-06-22 21:09:19 -0400

    GamePackage

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement(){
+        if(!isLocalPlayer){return;}
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal * .05f, moveVertical * .05f, 0);
+        transform.position = transform.position + movement;
+
+    }
+
+    void Update(){
+        HandleMovement();
+    }
+}
